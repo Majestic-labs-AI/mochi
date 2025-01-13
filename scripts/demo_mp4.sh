d06be7f0a9f650ab6bcc3dd4a27d54957c1a8909
Brian Cunnie <cunnie@majestic-labs.ai>
2025-01-13 09:30:19 -0800

    Script to generate different-sized videos
    - approximately 3 seconds (97 frames) because that seemed to strike the
      balance betweeen long-enough-to-view and
      short-enough-to-generate-in-a-reasonable-time
    - 9:16 (portrait) because that's what Instagram recommends
    - height & width must be divisible by 16 (Mochi)
    
    Signed-off-by: Brian Cunnie <cunnie@majestic-labs.ai>

---
+++ scripts/demo_mp4.sh
@@ -0,0 +1,26 @@
+
+# We set the resolution; which need to be a multiple of 16.
+# Also, Instagram recommends 9:16 (portrait, not landscape)
+# so the minimal width is 9 x 16 = 144, second minimal is
+# (9 + 9) x 16 = 288, etc.
+set -- \
+    256 144 \
+    512 288 \
+    768 432 \
+    1024 576 \
+    1280 720
+
+mochi_prompt="A rose budding then blossoming"
+num_frames=97
+while [ $# -gt 0 ]; do
+    height=$1
+    width=$2
+    python3 ./demos/cli.py \
+        --model_dir weights/ \
+        --cpu_offload \
+        --num_frames $num_frames \
+        --width $width \
+        --height $height \
+        --prompt "$mochi_prompt"
+    shift 2
+done
